b8aca336729232cc45255a1ca19b94d793ce9d80
Chris Kopel <ckopel@Chriss-MacBook-Pro.local>
2024-11-12 11:11:20 -0700

    updated and tested library for uploading to gsheet

--- 2024_10_28__all_in_one/library/w_scrape_org.py
+++ 2024_10_28__all_in_one/library/w_scrape_org.py
@@ -23,6 +23,7 @@ import pytz
 
 import requests
 
+import gspread
 
 
 
@@ -37,14 +38,22 @@ class scrape_w:
 
 
         # Define the project directories
-        project_base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+        project_base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
         self.data_base_dir = os.path.join(project_base_dir, "data")
+        self.metadata_base_dir = os.path.join(self.data_base_dir, "metadata")
 
         ## Create the full path to the metadata files
-        self.fb_md_fp = os.path.join(self.data_base_dir, "football_data.csv")
-        self.nba_md_fp = os.path.join(self.data_base_dir, "nba_team_abbr.csv")
-        self.nhl_md_fp = os.path.join(self.data_base_dir, "nhl_team_abbr.csv")
-        self.nfl_md_fp = os.path.join(self.data_base_dir, "nfl_espn_team_abbr.csv")
+        self.fb_md_fp = os.path.join(self.metadata_base_dir, "football_data.csv")
+        self.nba_md_fp = os.path.join(self.metadata_base_dir, "nba_team_abbr.csv")
+        self.nhl_md_fp = os.path.join(self.metadata_base_dir, "nhl_team_abbr.csv")
+        self.nfl_md_fp = os.path.join(self.metadata_base_dir, "nfl_espn_team_abbr.csv")
+
+        ## FPs for each destination folder
+        self.fb_folder_path = os.path.join(self.data_base_dir, "fb")  # Create the path to the fb folder within data_base_dir
+        self.nba_folder_path = os.path.join(self.data_base_dir, "nba")  # Create the path to the nba folder within data_base_dir
+        self.nhl_folder_path = os.path.join(self.data_base_dir, "nhl")  # Create the path to the nhl folder within data_base_dir
+        self.nfl_folder_path = os.path.join(self.data_base_dir, "nfl")  # Create the path to the nfl folder within data_base_dir
+        self.master_future_folder_path = os.path.join(self.data_base_dir, "master_future")
 
 
         ## misc. vars
@@ -99,69 +108,75 @@ class scrape_w:
         team_url_dict = {team: url + 'matches/' for team, url in team_url_dict.items()} # Amend each URL by adding 'matches/' at the end
 
 
-        for team, url in team_url_dict.items():
+        for fb_team, fb_url in team_url_dict.items():
 
-            try:
+            df_team_fixtures = self.retrieve_fb_schedule(fb_team, fb_url)
 
-                # Setup WebDriver
-                service = Service(self.chrome_driver_path)  # Use the path from environment variable
-                driver = webdriver.Chrome(service=service)
-
-                # Open the page
-                driver.get(url)
-                
-                # Wait until the cookie popup is present and clickable
-                WebDriverWait(driver, 5).until(
-                    EC.element_to_be_clickable((By.XPATH, "//button[text()='Reject All']"))
-                ).click()
-
-                # Now proceed with your scraping task
-                ## Use BeautifulSoup to parse the page source once the page is fully loaded
-                soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-                # Find the table and extract data as before
-                table = soup.find_all('table', class_='matches')
-                dates = [row.text for row in table[0].find_all('td', class_="full-date")]
-                leagues = [row.text.strip() for row in table[0].find_all('td', class_="competition")]
-                homes = [row.text.strip() for row in table[0].find_all('td', class_="team")[::2]]
-                aways = [row.text.strip() for row in table[0].find_all('td', class_="team")[1::2]]
-                times = [row.text.strip() for row in table[0].find_all('td', class_="score-time")]
-
-                # Create dataframes
-                df_team_fixtures = pd.DataFrame(
-                    {
-                        'Team': team,
-                        'Date': dates,
-                        'League': leagues,
-                        'Home Team': homes,
-                        'Time': times,
-                        'Away Team': aways
-                    }
-                )
-
-                ## Add to master df
-                if 'df_fb_master' in locals():
-                    df_fb_master = pd.concat([df_fb_master, df_team_fixtures], ignore_index=True)
-                else:
-                    df_fb_master = df_team_fixtures.copy()
-
-
-            except TimeoutException:
-                print("Timed out waiting for cookie pop-up or other elements")
-                
-            finally:
-                # Close the browser
-                driver.quit()
-
+            ## Add to master df
+            if 'df_fb_master' in locals():
+                df_fb_master = pd.concat([df_fb_master, df_team_fixtures], ignore_index=True)
+            else:
+                df_fb_master = df_team_fixtures.copy()
 
         ## Save the df
         # Create the full path for the CSV file
         fb_file_name = f"df_fb_scrape_{self.today_str}.csv"
-        fb_folder_path = os.path.join(self.data_base_dir, "fb")  # Create the path to the fb folder within data_base_dir
-        fb_file_path = os.path.join(fb_folder_path, fb_file_name)
+        fb_file_path = os.path.join(self.fb_folder_path, fb_file_name)
 
         # Save the DataFrame
         df_fb_master.to_csv(fb_file_path, index=False)
+
+
+    # Get the schedule via selenium
+    def retrieve_fb_schedule(self, fb_team, fb_url):
+        try:
+
+            # Setup WebDriver
+            service = Service(self.chrome_driver_path)  # Use the path from environment variable
+            driver = webdriver.Chrome(service=service)
+
+            # Open the page
+            driver.get(fb_url)
+            
+            # Wait until the cookie popup is present and clickable
+            WebDriverWait(driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[text()='Reject All']"))
+            ).click()
+
+            # Now proceed with your scraping task
+            ## Use BeautifulSoup to parse the page source once the page is fully loaded
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+            # Find the table and extract data as before
+            table = soup.find_all('table', class_='matches')
+            dates = [row.text for row in table[0].find_all('td', class_="full-date")]
+            leagues = [row.text.strip() for row in table[0].find_all('td', class_="competition")]
+            homes = [row.text.strip() for row in table[0].find_all('td', class_="team")[::2]]
+            aways = [row.text.strip() for row in table[0].find_all('td', class_="team")[1::2]]
+            times = [row.text.strip() for row in table[0].find_all('td', class_="score-time")]
+
+            # Create dataframes
+            df_team_fixtures = pd.DataFrame(
+                {
+                    'Team': fb_team,
+                    'Date': dates,
+                    'League': leagues,
+                    'Home Team': homes,
+                    'Time': times,
+                    'Away Team': aways
+                }
+            )
+
+            return df_team_fixtures
+
+
+        except TimeoutException:
+            driver.quit()
+            print(f"Timed out waiting for cookie pop-up or other elements for {fb_team} at {fb_url}")
+            
+        finally:
+            # Close the browser
+            driver.quit()
 
     # End FB
     #######################################################
@@ -192,8 +207,7 @@ class scrape_w:
         ## Save the df
         # Create the full path for the CSV file
         nba_file_name = f"df_nba_scrape_{self.today_str}.csv"
-        nba_folder_path = os.path.join(self.data_base_dir, "nba")  # Create the path to the nba folder within data_base_dir
-        nba_file_path = os.path.join(nba_folder_path, nba_file_name)
+        nba_file_path = os.path.join(self.nba_folder_path, nba_file_name)
 
         # Save the DataFrame
         df_nba_master.to_csv(nba_file_path, index=False)
@@ -233,7 +247,7 @@ class scrape_w:
             df['user_team'] = self.nba_md['team'][self.nba_md['abbr'] == nba_team_abbr].iloc[0]
             
             # Create new cols from fns
-            df['game_type'] = np.where(df['Notes'] == 'In-Season Tournament', 'In-Season Tournament', df['game_type']) # Apply conditional update to game_type column
+            df['game_type'] = np.where(df['Notes'] == 'In-Season Tournament', 'In-Season Tournament', 'Regular Season') # Apply conditional update to game_type column
 
             return df
         
@@ -270,8 +284,7 @@ class scrape_w:
         ## Save the df
         # Create the full path for the CSV file
         nhl_file_name = f"df_nhl_scrape_{self.today_str}.csv"
-        nhl_folder_path = os.path.join(self.data_base_dir, "nhl")  # Create the path to the nhl folder within data_base_dir
-        nhl_file_path = os.path.join(nhl_folder_path, nhl_file_name)
+        nhl_file_path = os.path.join(self.nhl_folder_path, nhl_file_name)
 
         # Save the DataFrame
         df_nhl_master.to_csv(nhl_file_path, index=False)
@@ -308,6 +321,7 @@ class scrape_w:
             df['computer_date'] = pd.to_datetime(df['Date'])
             df['Date'] = df['computer_date'].dt.strftime('%a, %b %d, %Y')
             df['user_team'] = self.nhl_md['team'][self.nhl_md['abbr'] == nhl_team_abbr].iloc[0]
+            df['game_type'] = 'Regular Season'
             
             return df
         
@@ -324,9 +338,9 @@ class scrape_w:
     # Begin NFL
     def scrape_nfl(self):
 
-        nfl_md = pd.read_csv(self.nfl_md_fp)
+        self.nfl_md = pd.read_csv(self.nfl_md_fp)
 
-        for nfl_team in nfl_md['abbr']:
+        for nfl_team in self.nfl_md['abbr']:
             
             # Scrape
             nfl_team_sch = self.retrieve_nfl_schedule(nfl_team)
@@ -344,8 +358,7 @@ class scrape_w:
         ## Save the df
         # Create the full path for the CSV file
         nfl_file_name = f"df_nfl_scrape_{self.today_str}.csv"
-        nfl_folder_path = os.path.join(self.data_base_dir, "nfl")  # Create the path to the nfl folder within data_base_dir
-        nfl_file_path = os.path.join(nfl_folder_path, nfl_file_name)
+        nfl_file_path = os.path.join(self.nfl_folder_path, nfl_file_name)
 
         # Save the DataFrame
         df_nfl_master.to_csv(nfl_file_path, index=False)
@@ -355,7 +368,7 @@ class scrape_w:
     def retrieve_nfl_schedule(self, nfl_team_abbr):
 
         # URL of the website
-        nfl_url = f"https://www.pro-football-reference.com/teams/{nfl_team_abbr}/2024/gamelog/"
+        nfl_url = f"https://www.espn.com/nfl/team/schedule/_/name/{nfl_team_abbr}"
 
         headers = {
             "User-Agent": "YourAppName/1.0 (https://yourwebsite.com; contact@yourwebsite.com)"
@@ -367,23 +380,40 @@ class scrape_w:
         # Check if the request was successful
         if response.status_code == 200:
             time.sleep(4) # if successful, wait n secs before reading in the html
-            dfs_list = pd.read_html(nfl_url)
+            df = pd.read_html(nfl_url)[0]
 
-            # Concatenate all DataFrames in dfs_list
-            df = pd.concat(dfs_list, ignore_index=True)
+            # Find the index of the row that starts with "WK", "DATE", etc.
+            start_row = df[(df[0] == "WK") & (df[1] == "DATE") & (df[2] == "OPPONENT") & (df[3] == "TIME")].index[0]
 
-            # Flatten the multi-level columns, ignoring 'Unnamed' and removing '_level_'
-            df.columns = [
-                re.sub(r'\d+\s', '', ' '.join(col).strip().replace("Unnamed: ", "").replace("_level_", "")).replace("0", "").replace("1", "").replace("6","Home/Away").replace("4","Result")
-                for col in df.columns
-            ]
+            # Find the index of the row that starts with "Preseason"
+            end_row = df[df[0] == "Preseason"].index[0]
 
-            df = df.drop(columns='3')
+            # Select rows between the start and end rows, and set the start_row as the header
+            df = df.iloc[(start_row-1) + 1:end_row].reset_index(drop=True)  # +1 to exclude the header row from the data
+            df.columns = df.iloc[0]  # Set the first row of the selection as column headers
+            df = df[1:]  # Drop the header row itself
 
-            # Organize
-            df['Home/Away'] = df['Home/Away'].fillna("vs.")
+            # Filter out rows where any cell has "BYE WEEK"
+            df = df[~df.eq("BYE WEEK").any(axis=1)]
+
+            # Drop the first column and the last two columns
+            df = df.iloc[:, :-2].reset_index(drop=True)
+
+            # Extract "vs" or "@" into a new column "Home/Away"
+            df['Home/Away'] = df['OPPONENT'].str.extract(r'(vs|@)')
+            df['Home/Away'] = df['Home/Away'].replace('vs', 'vs.')
+
+            # Remove "vs" or "@" from the "OPPONENT" column and strip whitespace
+            df['OPPONENT'] = df['OPPONENT'].str.replace(r'vs |@ ', '', regex=True).str.strip()
+
+            ## Organize
             df['user_team'] = self.nfl_md['team'][self.nfl_md['abbr'] == nfl_team_abbr].iloc[0]
             df['game_type'] = "regular season"
+            df = df.drop(columns=df.filter(regex="^tickets$").columns)
+            df['TIME'] = df['TIME'].replace("TBD", pd.NA)
+
+            # Replace "TBD" with NaN using loc
+            df.loc[df['DATE'].str.contains('TBD'), 'DATE'] = np.nan
 
             return df
         
@@ -425,10 +455,10 @@ class scrape_w:
         nfl_date_parser = lambda x: pd.to_datetime(f"{x}, {next_year}" if "Jan" in x else f"{x}, {current_year}", format="%a, %b %d, %Y").date() if pd.notna(x) else np.nan
 
         # Master data files for each subject
-        fb_master_fp = os.path.join(self.data_base_dir, fb_master_filename)
-        nba_master_fp = os.path.join(self.data_base_dir, nba_master_filename)
-        nhl_master_fp = os.path.join(self.data_base_dir, nhl_master_filename)
-        nfl_master_fp = os.path.join(self.data_base_dir, nfl_master_filename)
+        fb_master_fp = os.path.join(self.fb_folder_path, fb_master_filename)
+        nba_master_fp = os.path.join(self.nba_folder_path, nba_master_filename)
+        nhl_master_fp = os.path.join(self.nhl_folder_path, nhl_master_filename)
+        nfl_master_fp = os.path.join(self.nfl_folder_path, nfl_master_filename)
 
 
         # Read in files with their dates
@@ -442,7 +472,7 @@ class scrape_w:
         df_fb['Date'] = pd.to_datetime(df_fb['Date']).dt.date
         df_fb = df_fb[df_fb['Date'] >= self.today]
         df_fb['Time'] = df_fb['Time'].astype(str).str.replace(r'\s+', '', regex=True)
-        df_fb['Time'] = df_fb['Time'].replace('-', pd.NA)
+        df_fb['Time'] = df_fb['Time'].replace('-', pd.NA).replace('SUSP', pd.NA)
         df_fb['Time'] = pd.to_datetime(df_fb['Time'], format='%H:%M').dt.time
 
 
@@ -551,4 +581,43 @@ class scrape_w:
 
 
     # End organizing the data
+    ##############################################################################################################
+
+
+
+
+    ##############################################################################################################
+    # Begin Uploading the data to GSheets
+
+    def upload_to_gsheet(self, csv):
+        ### Send to GSheet
+        ## Config details
+        sa_path_windows = r"C:\Users\Owner\AppData\Local\Programs\Python\Python310\Lib\site-packages\gspread\service_account.json"
+        sa_path_mac = "/Users/ckopel/Documents/keys/service_account.json"
+        sa_path = sa_path_windows if os.name == "nt" else sa_path_mac if os.name == "posix" else None
+
+        gc = gspread.service_account(filename=sa_path)
+
+
+        ## Read in the csv
+        master_future_fp = os.path.join(self.master_future_folder_path, csv)
+        df = pd.read_csv(master_future_fp)
+
+        # Replace NaN and infinite values
+        df = df.replace([float('inf'), -float('inf')], None)  # Replace infinite values with None
+        df = df.fillna('')  # Replace NaN with an empty string
+
+
+        ## Open the sheet
+        sh = gc.open("project_w_landing")
+        worksheet = sh.worksheet("Data")  # Access the "Data" sheet
+        worksheet.clear() # Clear the sheet
+
+
+        # Upload CSV data to Google Sheets
+        worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+        
+
+
+    # End uploading the data to GSheets
     ##############################################################################################################
